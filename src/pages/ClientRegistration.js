e57057e823c73fb304ab47b34fb5c461066127c1
mohammed MOHIUDDIN <coolmohhi@gmail.com>
2024-02-17 03:21:19 -0600

    added client-registration and login pages

--- src/pages/ClientRegistration.js
+++ src/pages/ClientRegistration.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function ClientRegistration() {
+  //hooks
+  const [email, setEmail] = useState("");
+  const [password, setPass] = useState("");
+
+  //handler
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Signing up with:", { email, password });
+  };
+
   return (
-	  <div>ClientRegistration</div>
-  )
+    <form onSubmit={handleSubmit}>
+      <h2>Sign Up</h2>
+      <div>
+        <label>Email: </label>
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="youremail@gmail.com"
+          required
+        ></input>
+        <br />
+        <label>Password: </label>
+        <input
+          value={password}
+          onChange={(e) => setPass(e.target.value)}
+          type="password"
+          placeholder="********"
+          required
+        ></input>
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
+  );
 }
 
-export default ClientRegistration
+export default ClientRegistration;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
-	return (
-		<div>Login</div>
-	)
+  //hooks
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  //handler
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({ password });
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div>
+          <label>Email: </label>
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type="email"
+            placeholder="youremail@gmail.com"
+            required
+          ></input>
+          <br />
+          <label>Password: </label>
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+            placeholder="********"
+            required
+          ></input>
+        </div>
+        <button type="submit">Log In</button>
+      </form>
+      <p>
+        Don't have an account? <Link to="/client-registration">Sign Up</Link>
+      </p>
+    </>
+  );
 }
 
-export default Login
+export default Login;
