6ef182550fa3ca79b4c0c7d8ee595ba62705b146
StevenWeng60 <stevenweng60@gmail.com>
2024-04-12 20:11:52 -0500

    Added unit testing for fuel history and fuel quote

--- tests/fuelhistory.test.js
+++ tests/fuelhistory.test.js
@@ -1,11 +1,22 @@
-const request = require("supertest");
+const session = require("supertest-session");
 const app = require("../app")
 
+
 describe("GET /fuelhistory", () => {
-  it("Should return a response with an array of fuel quotes", async () => {
-    const response = await request(app).get("/fuelhistory");
-
-    expect(response.statusCode).toBe(200);
-
+  beforeEach(function () {
+    testSession = session(app);
   });
-})
+
+  it("Should return a response with an array of fuel quotes", async () => {
+      // Set the session variable
+      await testSession.post('/set-session')
+        .send({ ClientInformationID: 2 })
+        .expect(200);
+
+      // Now make the GET request
+      const response = await testSession.get("/fuelhistory");
+
+      expect(response.statusCode).toBe(200);
+      // Add more assertions here based on the expected response body
+  }, 30000);
+});
