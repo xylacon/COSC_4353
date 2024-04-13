5bceb8d680880317aaecb4541fb76632f13dd46b
coolMohi <coolmohhi@gmail.com>
2024-04-12 22:24:08 -0500

    typo fix

--- tests/client-profileget.test.js
+++ tests/client-profileget.test.js
@@ -32,21 +32,3 @@ describe("GET /client-profile", () => {
     expect(response.statusCode).toBe(200);
   }, 30000);
 });
-
-// it("should return 404 if client profile does not exist", async () => {
-//     await testSession
-//       .post("/set-session")
-//       .send({ ClientInformationID: 1000000000000 })
-//       .expect(200);
-
-//     const response = await testSession.post("/client-profile").send({
-//       name: "John Doe",
-//       address1: "123 Main St",
-//       address2: "",
-//       city: "Houston",
-//       state: "TX",
-//       zip: "77006",
-//     });
-//     expect(response.status).toBe(404);
-//     expect(response.text).toBe("Client profile not found");
-//   });
